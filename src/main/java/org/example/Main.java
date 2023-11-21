9b75c32f671cdf056694a5c20aa24f7582bf1407
CarlosMateoM <carlosmateo484@gmail.com>
2023-11-21 00:10:58 -0500

    refactor: main structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,15 @@ import org.wrs.configArduino.ArduinoController;
 import org.wrs.configArduino.SerialComunicationInterface;
 import org.wrs.configArduino.SerialLector;
 import org.wrs.connectionFactory.ConnectionFactory;
+import org.wrs.controllers.RechargeController;
 import org.wrs.controllers.StudentController;
+import org.wrs.dao.RechargeDao;
 import org.wrs.dao.StudentDao;
 
 import javax.swing.*;
 
 import org.wrs.view.LogView;
+import org.wrs.view.RechargeView;
 import org.wrs.view.SellView;
 import org.wrs.view.View;
 
@@ -29,28 +32,32 @@ public class Main {
         }
 
 
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
         //create serial lector and init arduino connection
         SerialLector serialLector = new SerialLector(new PanamaHitek_Arduino());
         serialLector.startConnectionArduino();
+        ArduinoController arduinoController = new ArduinoController(serialLector);
 
-
-
+        //student view and controller
         View vista = new View();
-        LogView logView = new LogView();
-        SellView overviewView = new SellView();
-
-        ConnectionFactory connectionFactory = new ConnectionFactory();
         StudentDao studentDao = new StudentDao(connectionFactory.getConnection());
-
         StudentController controller = new StudentController(studentDao, vista);
 
-        ArduinoController arduinoController = new ArduinoController(serialLector);
+        //recharge view and controller
+        RechargeView rechargeView = new RechargeView(vista);
+        RechargeDao rechargeDao = new RechargeDao(connectionFactory.getConnection());
+        RechargeController rechargeController = new RechargeController(studentDao,rechargeDao, rechargeView);
+
+
+
+
+        vista.setActionListener(rechargeController);
         vista.setActionListener(arduinoController);
 
 
         controller.initView();
 
-        //PanamaHitek_Arduino pha = new PanamaHitek_Arduino();
 
         vista.setVisible(true);
 
@@ -63,15 +70,10 @@ public class Main {
             }
         };
 
-        SerialComunicationInterface iRecharge = new SerialComunicationInterface() {
-            @Override
-            public void setValueArduino(String data) {
-                JOptionPane.showMessageDialog(null, data);
-            }
-        };
+
 
         serialLector.setPurchaseInterface(iPurchase);
-        serialLector.setRechargeInterface(iRecharge);
+        serialLector.setRechargeInterface(rechargeController);
 
 
     }
